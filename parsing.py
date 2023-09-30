0201a36b1f692fc57b5272101445f0fbe76eb23c
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-09-30 11:35:19 +0300

    реализована очистка расписания от дублирующихся предметов

--- parsing.py
+++ parsing.py
@@ -25,7 +25,25 @@ def week_timetable_dict(url: str, next_week: bool = False) -> dict[str, list]:
     return timetable
 
 
+def removing_unnecessary_items(timetable: dict[str, list]) -> dict[str, list]:
+    result = dict()
+    for key, value in timetable.items():
+        result[key] = list()
+        for i in range(len(value)):
+            if i == 0 and len(value) == 1:
+                result[key] += [value[i]]
+            elif i + 1 < len(value):
+                if value[i][0] != value[i + 1][0] or \
+                        (value[i][0] == value[i + 1][0] and
+                         (len(result[key]) == 0 or result[key][-1][0] != value[i][0])):
+                    result[key] += [value[i]]
+            else:
+                if i != 0 and value[i - 1][0] != value[i][0]:
+                    result[key] += [value[i]]
+    return result
+
+
 if __name__ == "__main__":
     links_file = open("links for parsing.txt")
     link = links_file.readline().strip()
-    pprint(week_timetable_dict(link, True))
+    pprint(removing_unnecessary_items(week_timetable_dict(link, True)))
