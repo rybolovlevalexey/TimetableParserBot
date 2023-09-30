7e45c97dc5649dd05899b92c885a482756942931
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-09-30 11:07:07 +0300

    парсинг спрятан в функцию, добавлена возможность получить расписание на следующую неделю

--- parsing.py
+++ parsing.py
@@ -1,19 +1,31 @@
 import requests
-import datetime
 import bs4
 from pprint import pprint
+from datetime import date, timedelta
 
 
-links_file = open("links for parsing.txt")
-url = links_file.readline().strip()
-responce = requests.get(url)
-html = bs4.BeautifulSoup(responce.content, "html.parser")
-timetable = dict()  # {day: [subjects with info]}
+def week_timetable_dict(url: str, next_week: bool = False) -> dict[str, list]:
+    if next_week:
+        today = date.today()
+        days_to_monday = (7 - today.weekday()) % 7
+        next_monday = today + timedelta(days=days_to_monday)
+        url += f"/{next_monday}"
+    responce = requests.get(url)
+    html = bs4.BeautifulSoup(responce.content, "html.parser")
+    timetable = dict()  # {day: [subjects with info]}
 
-for elem in html.find_all(name="div", attrs={"class": "panel panel-default"}):
-    block = bs4.BeautifulSoup(str(elem), "html.parser")
-    day = block.find(name="h4").text.strip()
-    timetable[day] = list()
-    for elem in block.select(".panel-collapse > .common-list-item"):
-        info = list(filter(lambda x: len(x) > 0, map(lambda x: x.strip(), elem.text.strip().split("\n"))))
-        timetable[day].append(info)
+    for elem in html.find_all(name="div", attrs={"class": "panel panel-default"}):
+        block = bs4.BeautifulSoup(str(elem), "html.parser")
+        day = block.find(name="h4").text.strip()
+        timetable[day] = list()
+        for elem in block.select(".panel-collapse > .common-list-item"):
+            info = list(filter(lambda x: len(x) > 0,
+                               map(lambda x: x.strip(), elem.text.strip().split("\n"))))
+            timetable[day].append(info)
+    return timetable
+
+
+if __name__ == "__main__":
+    links_file = open("links for parsing.txt")
+    link = links_file.readline().strip()
+    pprint(week_timetable_dict(link, True))
