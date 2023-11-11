55e4fe56794aa46e634a671f6b703ddf1f861d9e
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2023-11-11 11:43:58 +0300

    создана основа для Api на Flask, получение расписания по имени пользователя, предварительно сохранив его группу. Реализованы методы get и post

--- api_interface.py
+++ api_interface.py
@@ -1,5 +1,10 @@
 import requests
 
+# добавление нового пользователя
+# post_data = {"name": "alex", "group": "22.Б07-мм"}
+# post_result = requests.post("http://127.0.0.1:3000/api", data=json.dumps(post_data),
+#                             headers={'Content-Type': 'application/json'})
+
+# получение расписания по логину пользователя
 result = requests.get("http://127.0.0.1:3000/api/alex")
-post_result = requests.post("http://127.0.0.1:3000/api:", data={})
 print(result.text)
--- parser_api.py
+++ parser_api.py
@@ -1,5 +1,7 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
+from models import WebUser, GroupDirection
+from parsing import week_timetable_dict
 
 app = Flask(__name__)
 api = Api()
@@ -7,14 +9,28 @@ api = Api()
 
 class BaseSchedule(Resource):
     def get(self, user_name):
-        return user_name
+        db_result = WebUser.select().where(WebUser.login == user_name)
+        if db_result.count() == 0:
+            return {"user_name": user_name, "response": "User not found"}
+        user_group = list(elem.group_name for elem in db_result)[0]
+        group_url = list(elem.url for elem in GroupDirection.select().\
+            where(GroupDirection.group_name == user_group))[0]
+        return week_timetable_dict(group_url)
 
-    def post(self, user_name, user_group):
-        pass
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("name", type=str)
+        parser.add_argument("group", type=str)
+        args = parser.parse_args()
+
+        if WebUser.select(WebUser.group_name).where(WebUser.login == args["name"]).count() == 0:
+            WebUser.create(login=args["name"], group_name=args["group"])
+        else:
+            WebUser.update(group_name=args["group"]).where(WebUser.login == args["name"]).execute()
+        return {"done": True}
 
 
-api.add_resource(BaseSchedule, "/api/<string:user_name>")
-api.add_resource(BaseSchedule, "/api/<string:user_name>/<string:user_group>")
+api.add_resource(BaseSchedule, "/api", "/api/<string:user_name>")
 api.init_app(app)
 
 if __name__ == "__main__":
