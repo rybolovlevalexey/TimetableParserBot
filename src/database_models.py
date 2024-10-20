d4388f6b0d4bff946f665705e16182c75ebf5fe1
Alexey <rybolovlevalexey@yandex.ru>
2024-10-20 12:28:31 +0300

    feat: добавлена база данных, alembic и первые модели БД

--- src/database_models.py
+++ src/database_models.py
@@ -1,10 +1,12 @@
-from sqlalchemy import Integer
+from sqlalchemy import Integer, String, Boolean
 from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
 from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
 
-from config import settings
+from src.config import settings
 
-engine = create_async_engine(settings.DB_URL)
+DB_URL: str = settings.get_db_url()
+
+engine = create_async_engine(DB_URL)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 
@@ -13,5 +15,12 @@ class Base(AsyncAttrs, DeclarativeBase):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
 
 
+# Основная база данных пользователей с общей информацией и необходимым минимумом для авторизации
 class User(Base):
-    pass
+    __tablename__ = "users"
+
+    login: Mapped[str]
+    full_name: Mapped[str] = mapped_column(String, default="")
+    email: Mapped[str] = mapped_column(String)
+    disabled: Mapped[bool] = mapped_column(Boolean)
+    hashed_password: Mapped[str] = mapped_column(String)
