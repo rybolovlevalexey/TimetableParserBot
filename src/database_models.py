6205e495c860c78f10a03b8ca281c0e3c3bf2bb0
Alexey <rybolovlevalexey@yandex.ru>
2024-10-21 12:22:56 +0300

    feat: добавлены модели для работы с факультетами, образовательными направлениями на факультетах и группами

--- src/database_models.py
+++ src/database_models.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Integer, String, Boolean
-from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
+from sqlalchemy import Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped, relationship
 from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
+from sqlalchemy.ext.hybrid import hybrid_property
 
 from src.config import settings
 
@@ -15,7 +16,7 @@ class Base(AsyncAttrs, DeclarativeBase):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
 
 
-# Основная база данных пользователей с общей информацией и необходимым минимумом для авторизации
+# Основная таблица базы данных пользователей с общей информацией и необходимым минимумом для авторизации
 class User(Base):
     __tablename__ = "users"
 
@@ -24,3 +25,43 @@ class User(Base):
     email: Mapped[str] = mapped_column(String)
     disabled: Mapped[bool] = mapped_column(Boolean)
     hashed_password: Mapped[str] = mapped_column(String)
+
+
+# Таблица с информацией о факультетах
+class Faculty(Base):
+    __tablename__ = "faculties"
+
+    name: Mapped[str] = mapped_column(String, unique=True)
+    url: Mapped[str] = mapped_column(String, unique=True)
+    # связь с моделью EduProgram - список образовательных программ этого факультета
+    edu_programs = relationship("EduProgram", back_populates="faculty")
+
+
+# Таблица с информацией о направлениях
+class EduProgram(Base):
+    __tablename__ = "edu_programs"
+
+    name: Mapped[str] = mapped_column(String)
+    year: Mapped[str] = mapped_column(String)
+    url: Mapped[str] = mapped_column(String, unique=True)
+    # связь с моделью Faculty - имя факультета / модель факультета целиком (на котором находится данная программа)
+    faculty_name: Mapped[str] = mapped_column(String, ForeignKey("faculties.name"))
+    faculty = relationship("Faculty", back_populates="edu_programs")
+    # связь с моделью StudyGroup - список всех групп данной образовательной программы
+    groups = relationship("StudyGroup", back_populates="edu_program")
+
+    # создание полного названия образовательной программы = название + год поступления
+    @hybrid_property
+    def program_full_name(self) -> str:
+        return f"{self.name} {self.year}"
+
+
+# Таблица с информацией об образовательных группах
+class StudyGroup(Base):
+    __tablename__ = "studying_groups"
+
+    name: Mapped[str]
+    url: Mapped[str] = mapped_column(String, unique=True)
+    # связь с моделью EduProgram - название программы / модель программы целиком (к которой принадлежит данная группа)
+    edu_program_id: Mapped[int] = mapped_column(Integer, ForeignKey("edu_programs.id"))
+    edu_program = relationship("EduProgram", back_populates="groups")
